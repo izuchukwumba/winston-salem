f60496d18ef4a1b304862bec3bc818fbed153ae4
izuchukwumba <barth.poetic@gmail.com>
2025-03-27 22:56:22 -0500

    ui-fix-chatbox

--- frontend/src/pages/chat/ChatBox.tsx
+++ frontend/src/pages/chat/ChatBox.tsx
@@ -66,7 +66,7 @@ const Chat: React.FC = () => {
   };
 
   return (
-    <div className="min-h-[100%] max-h-[100%] flex flex-col relative overflow-hidden">
+    <div className="min-h-[100%] max-h-[100%] flex flex-col relative overflow-y-auto">
       {/* background image */}
       <div className="absolute inset-0  flex justify-center items-center pointer-events-none z-0">
         <img
@@ -87,7 +87,7 @@ const Chat: React.FC = () => {
             />
             <div className="absolute bottom-1 right-0 w-3 h-3 bg-green-600 rounded-full"></div>
           </div>
-          <div className="font-[inter] font-bold">Lindaa</div>
+          <div className="font-[inter] font-bold">Linda</div>
         </div>
       </div>
 
