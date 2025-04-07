e17110cf027bd13b3d5df1fa047bb44075e5d562
izuchukwumba <barth.poetic@gmail.com>
2025-04-07 15:41:43 -0500

    deployment fixes

--- city/src/pages/HomePage.tsx
+++ city/src/pages/HomePage.tsx
@@ -22,7 +22,15 @@ const HomePage: React.FC = () => {
           </div>
           <div className="flex flex-col justify-center items-center gap-y-10 mb-20">
             <div className="flex flex-col lg:flex-row justify-center items-center gap-y-6 lg:gap-y-0 lg:gap-x-14 w-full text-lg lg:text-xl">
-              <div className="bg-[#25228B] text-white font-bold font-playfair px-12 py-2 rounded-md cursor-pointer">
+              <div
+                className="bg-[#25228B] text-white font-bold font-playfair px-12 py-2 rounded-md cursor-pointer"
+                onClick={() => {
+                  window.open(
+                    "https://winston-salem-iota.vercel.app/",
+                    "_blank"
+                  );
+                }}
+              >
                 I need help
               </div>
               <div
